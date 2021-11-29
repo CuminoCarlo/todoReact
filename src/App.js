1aa352bc3865f80cf2aa0284bf2b9e2a4ba0d001
Carlo Cumino <ccumino89@gmail.com>
2021-11-29 23:02:45 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -50,7 +50,6 @@ function App(props) {
         3 tasks remaining
       </h2>
       <ul
-        role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
